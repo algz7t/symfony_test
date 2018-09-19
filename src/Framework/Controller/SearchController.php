4e3c0f2df79f990137cbf7c58cdc73ab9f5a740f
Alvin <leninbooter@gmail.com>
2018-09-20 01:02:09 +0200

    Test search controller

--- src/Framework/Controller/SearchController.php
+++ src/Framework/Controller/SearchController.php
@@ -1,6 +1,7 @@
 <?php
 namespace App\Framework\Controller;
 
+use App\Framework\Services\RecipeServices;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
@@ -10,12 +11,20 @@ use Symfony\Component\Routing\Annotation\Route;
  */
 class SearchController extends Controller
 {
+    /** @var RecipeServices $recipeServices */
+    protected $recipeServices;
+
+    public function __construct(RecipeServices $recipeServices)
+    {
+        $this->recipeServices = $recipeServices;
+    }
+
     /**
      * @Route("/search", name="search")
      */
     public function getSearchAction(Request $request)
     {
-
+        return $this->recipeServices->searchRecipesBySearchQuery("omelet");
         return $this->json(array('username' => 'jane.doe'));
     }
 }
