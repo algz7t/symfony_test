3b9653cc0bce6142bef9c0ddf021c4a322f059aa
Alvin <leninbooter@gmail.com>
2018-09-21 00:06:34 +0200

    Add dependencies / Bundles

--- config/bundles.php
+++ config/bundles.php
@@ -5,4 +5,6 @@ return [
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
     EightPoints\Bundle\GuzzleBundle\EightPointsGuzzleBundle::class => ['all' => true],
     Symfony\Bundle\WebServerBundle\WebServerBundle::class => ['dev' => true],
+    JMS\SerializerBundle\JMSSerializerBundle::class => ['all' => true],
+    FOS\RestBundle\FOSRestBundle::class => ['all' => true],
 ];
