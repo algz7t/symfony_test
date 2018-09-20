6fe02c2a30393f2d65a1f0cbb4946120ce796541
Alvin <leninbooter@gmail.com>
2018-09-21 00:07:52 +0200

    Add recipes endpoint

--- src/Framework/Controller/RecipesController.php
+++ src/Framework/Controller/RecipesController.php
@@ -2,7 +2,9 @@
 namespace App\Framework\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
+use FOS\RestBundle\Controller\Annotations\QueryParam;
 
 /**
  * @Route("/api")
@@ -11,9 +13,10 @@ class RecipesController extends Controller
 {
     /**
      * @Route("/recipes", name="index")
+     * @QueryParam
      */
     public function getRecipes()
     {
-        return $this->json(array('username' => 'jane.doe'));
+        return new JsonResponse();
     }
 }
