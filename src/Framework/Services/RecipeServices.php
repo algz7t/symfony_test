aa8190a8f85da421fa17c05c44cacb5828b36b7f
Alvin <leninbooter@gmail.com>
2018-09-21 00:07:10 +0200

    Add recipes service

--- src/Framework/Services/RecipeServices.php
+++ src/Framework/Services/RecipeServices.php
@@ -6,16 +6,26 @@ use Symfony\Component\DependencyInjection\ContainerInterface;
 
 class RecipeServices
 {
+    const RESPONSE_OK = 200;
+
     /** @var \GuzzleHttp\Client $client */
     protected $client;
 
     public function __construct(ContainerInterface $container)
     {
-        $this->client = $container->get('eight_points_guzzle.client.api_crm');
+        $this->client = $container->get('eight_points_guzzle.client.api_recipes_puppy');
     }
 
     public function searchRecipesBySearchQuery($query)
     {
-        return $this->client->get( '/', ['q' => $query]);
+        $response = $this->client->request('GET', '', [
+            'query' => ['q' => $query]
+        ]);
+
+        if(self::RESPONSE_OK === $response->getStatusCode()) {
+            return json_decode($response->getBody()->getContents());
+        }
+
+        return null;
     }
 }
