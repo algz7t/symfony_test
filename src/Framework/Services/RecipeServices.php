8da8415c4d3173e5f960c68825edd6e0664f5645
Alvin <leninbooter@gmail.com>
2018-09-21 02:55:17 +0200

    Clean recipes services

--- src/Framework/Services/RecipeServices.php
+++ src/Framework/Services/RecipeServices.php
@@ -7,6 +7,9 @@ use Symfony\Component\DependencyInjection\ContainerInterface;
 class RecipeServices
 {
     const RESPONSE_OK = 200;
+    const API_PAGING_QUERY_PARAMETER = 'p';
+    const API_QUERYSEARCH_QUERY_PARAMETER = 'q';
+    const API_INGREDIENTS_QUERY_PARAMETER = 'i';
 
     /** @var \GuzzleHttp\Client $client */
     protected $client;
@@ -18,18 +21,35 @@ class RecipeServices
 
     public function searchRecipesBySearchQuery($query, $page = null)
     {
-        $queryParams = ['q' => $query];
+        $queryParams = [self::API_QUERYSEARCH_QUERY_PARAMETER => $query];
 
         if($page) {
-            $queryParams['p'] = (int)$page;
+            $queryParams[self::API_PAGING_QUERY_PARAMETER] = (int)$page;
         }
 
-        $response = $this->client->request('GET', '', [
-            'query' => $queryParams
-        ]);
+        $response = $this->buildGetRequest($queryParams);
 
         if(self::RESPONSE_OK === $response->getStatusCode()) {
-            return json_decode($response->getBody()->getContents());
+            return $this->extractResults($response->getBody()->getContents());
+        }
+
+        return null;
+    }
+
+    private function buildGetRequest($queryParams = [])
+    {
+        return $this->client->request(
+            'GET',
+            '',
+            ['query' => $queryParams]
+        );
+    }
+
+    private function extractResults($responseContent)
+    {
+        $decodedResponse = json_decode($responseContent, true);
+        if(isset($decodedResponse['results'])) {
+            return $decodedResponse['results'];
         }
 
         return null;
